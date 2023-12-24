035ce448b4182ff526cef438d0bc892fc88e2ac5
Eduard Minosyan <Edu.min@mail.ru>
2023-12-25 01:17:09 +0400

    add Less1:Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿int a = 15;
+int b = 7;
+int c = 9;
+int d = 4;
+int e = 10;
+
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
